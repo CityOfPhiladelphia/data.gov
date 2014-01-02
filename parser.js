fc05b836dd3dcab57e5e62e47a89f07522a2bc5b
hdo272 <hillaryjiado@gmail.com>
2014-01-02 16:18:34 -0500

    hello

--- parser.js
+++ parser.js
@@ -1,18 +1,35 @@
 var request = require('request');
+var fs = require('fs');
 
 request('http://www.opendataphilly.org/api/resources/', function (error, response, body) {
   if (!error && response.statusCode == 200) {
   	var resources = JSON.parse(body);
-  	var newResources = [];
+	var newResources = [];
+	//iterates over each object
   	for(var i=0; i<resources.length; i++) {
   		var newResource = {};
-  		if(resources[i].organization == "City of Philadelphia") {
-  			newResource.Title = resources[i].name;
-  			newResource.Description = resources[i].short_description;
-  			newResources.push(newResource);
-  		}
+		var org = resources[i].organization.toLowerCase();
+		  //if resource's organization is city of philadelphia
+		  if(org == "city of philadelphia" || org == "city of philadelphia ") {
+			  //rename properties to conform with CCMS
+			  newResource.title = resources[i].name;
+			  newResource.description = resources[i].short_description;
+			  newResource.keyword = resources[i].tags[0].name;
+			  newResource.modified = resources[i].release_date;
+			  newResource.publisher = resources[i].organization;
+			  newResources.push(newResource);
+		  }
   	}
-  	console.log(newResources);
+	console.log(resources[i].tags[0].name);
+	fs.writeFile("CCMSdata.json", JSON.stringify(newResources), function(err) {
+	    if(err) {
+		console.log(err);
+	    } else {
+		console.log("The file was saved!");
+	    }
+	});
+	console.log(newResources);
   }
 });
+
 
