66f6fd3aef2d32563d2bbe133de94f2769bf7fb5
Mark Headd <mheadd@gmail.com>
2014-01-03 13:58:39 -0500

    Code refactoring.

--- parser.js
+++ parser.js
@@ -1,16 +1,39 @@
+// Include required modules.
 var request = require('request');
 var fs = require('fs');
 
-request('http://www.opendataphilly.org/api/resources/', function (error, response, body) {
+// Variable declarations.
+var apiEndpoint = 'http://www.opendataphilly.org/api/resources/';
+var dataFile = process.argv[2] || "CCMSdata.json";
+var debug = process.argv[3] || false;
+
+// Utility function to strip superfluous whitespace from string.
+function stripSpaces(str) {
+	return str.replace(/(^[\s]+|[\s]+$)/g, '');
+}
+
+// Fetch JSON from AI endpoint.
+request(apiEndpoint, function (error, response, body) {
+
   if (!error && response.statusCode == 200) {
+  	// Parse JSON from API.
   	var resources = JSON.parse(body);
+
+  	// New array to hold CCMS JSON objects.
 	var newResources = [];
-	//iterates over each resource
+
+	// Iterates over each element in the JSON array returned from the API.
   	for(var i=0; i<resources.length; i++) {
+  		
+  		// A new object to hold CCMS compliant JSON.
   		var newResource = {};
-		var org = resources[i].organization.toLowerCase();
-		  //if resource's organization is city of philadelphia
-		  if(org == "city of philadelphia" || org == "city of philadelphia ") {
+
+  		// The organization value from the source JSON
+		var org = stripSpaces(resources[i].organization.toLowerCase());
+
+		  // If resource's organization is city of philadelphia...
+		  if(org == "city of philadelphia") {
+
 			  //rename properties to conform with CCMS
 			  newResource.title = resources[i].name;
 			  newResource.description = resources[i].short_description;
@@ -22,18 +45,28 @@ request('http://www.opendataphilly.org/api/resources/', function (error, respons
 			  newResource.modified = resources[i].release_date;
 			  newResource.publisher = resources[i].organization;
 			  newResource.accessURL = resources[i].url;
+
+			  // Push new JSON object onto new resource array.
 			  newResources.push(newResource);
 		  }
   	}
-  	//write transformed JSON data, now in CCMS, to a file called CCMSdata.json
-	fs.writeFile("CCMSdata.json", JSON.stringify(newResources), function(err) {
+
+  	// Write transformed JSON data, now in CCMS, to a file called CCMSdata.json.
+	fs.writeFile(dataFile, JSON.stringify(newResources), function(err) {
 	    if(err) {
 		console.log(err);
 	    } else {
 		console.log("The file was saved!");
 	    }
 	});
-	console.log(newResources);
+	if(debug) {
+		console.log(newResources);
+	}	
+  }
+
+  // Otherwise, output error statement.
+  else {
+  	console.log("Could not fetch JSON data from API endpoint");
   }
 });
 
